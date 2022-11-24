d34f13ff0e3a94f62265e3e6bda18101874550a4
99518111 <118352373+99518111@users.noreply.github.com>
2022-11-24 14:08:29 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -5,15 +5,15 @@ def sum(a,b)
 
 def sub(a.b)
 {
-   echo "the difference of ${a} and is {b} ${a-b}" 
+   echo "the difference of ${a} and is ${b} ${a-b}" 
 }
 
 def mul(a,b)
 {
-   echo "the product of ${a} and is {b} ${a*b}"
+   echo "the product of ${a} and is ${b} ${a*b}"
 }
 
 def div(a.b)
 {
-  echo "the division of ${a} and is {b} ${a/b}"
+  echo "the division of ${a} and is ${b} ${a/b}"
 }
